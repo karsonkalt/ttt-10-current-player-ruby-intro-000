acb58f4a04b041ea3915daa0e2e545b791aba3b9
Karson Kalt <kars.kalt@gmail.com>
2020-08-10 16:33:09 +0000

    Automatically backed up by Learn

---
+++ lib/current_player.rb
@@ -0,0 +1,15 @@
+def turn_count(board)
+  counter = 0
+  board.each do |space|
+    if space == "X" || space == "O"
+      counter += 1
+    end
+  end
+end
+
+def current_player
+  if turn_count % 2
+    return "X"
+  else
+    return "O"
+end
