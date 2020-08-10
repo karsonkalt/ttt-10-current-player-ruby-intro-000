8d585fcfdf7f4d497aee675bd596855643004e58
Karson Kalt <kars.kalt@gmail.com>
2020-08-10 16:57:33 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -9,5 +9,9 @@ def turn_count(board)
 end
 
 def current_player(board)
-  turn_count(board).even? ? return "X" : return "0"
+  if turn_count(board).even?
+    return "X"
+  else
+    return "O"
+  end
 end
